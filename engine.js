aefb59050ad038eebd46bfbfdd23aadc437aec97
ciromellado <ciromellado@gmail.com>
2026-08-19 15:42:24 -0400

    Expand opening book with additional chess moves

--- engine.js
+++ engine.js
@@ -119,32 +119,158 @@ function iterativeDeepening(chess, maxDepth, timeLimit, color) {
   return bestMove;
 }
 
-// LIBRO DE APERTURAS (Respuestas instantáneas)
+// LIBRO DE APERTURAS EXPANDIDO (12 jugadas iniciales)
 const OPENING_BOOK = {
+  // Posición inicial
   'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1': ['e4', 'd4', 'Nf3', 'c4'],
+  
+  // 1. e4
   'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1': ['e5', 'c5', 'e6', 'c6', 'd5'],
+  
+  // 1. d4
   'rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq - 0 1': ['d5', 'Nf6', 'e6', 'c5'],
-  'rnbqkbnr/pppppppp/8/8/5N2/8/PPPPPPPP/RNBQKB1R b KQkq - 1 1': ['d5', 'Nf6', 'c5', 'e6'],
+  
+  // 1. Nf3
+  'rnbqkbnr/pppppppp/8/8/8/5N2/PPPPPPPP/RNBQKB1R b KQkq - 1 1': ['d5', 'Nf6', 'c5', 'e6'],
+  
+  // 1. c4
   'rnbqkbnr/pppppppp/8/8/2P5/8/PP1PPPPP/RNBQKBNR b KQkq - 0 1': ['e5', 'Nf6', 'c5', 'e6'],
+  
+  // 1. e4 e5
   'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2': ['Nf3', 'Bc4', 'Bb5', 'd4', 'Nc3'],
+  
+  // 1. e4 c5 (Siciliana)
   'rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2': ['Nf3', 'd4', 'Nc3', 'g3'],
+  
+  // 1. e4 e6 (Francesa)
   'rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2': ['d4', 'Nf3', 'Nc3'],
+  
+  // 1. e4 c6 (Caro-Kann)
   'rnbqkbnr/pp1ppppp/2p5/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2': ['d4', 'Nf3', 'Nc3'],
+  
+  // 1. d4 d5
   'rnbqkbnr/ppp1pppp/8/3p4/3P4/8/PPP1PPPP/RNBQKBNR w KQkq - 0 2': ['c4', 'Nf3', 'Nc3', 'Bg5'],
+  
+  // 1. d4 Nf6
   'rnbqkb1r/pppppppp/5n2/8/3P4/8/PPP1PPPP/RNBQKBNR w KQkq - 1 2': ['c4', 'Nf3', 'Bg5', 'Nc3'],
+  
+  // 1. e4 e5 2. Nf3
+  'rnbqkbnr/pppp1ppp/8/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2': ['Nc6', 'Nf6', 'd6', 'c6'],
+  
+  // 1. e4 e5 2. Bc4 (Italiana)
+  'rnbqkbnr/pppp1ppp/8/4p3/2B1P3/8/PPPP1PPP/RNBQK1NR b KQkq - 1 2': ['Nf6', 'Nc6', 'Bc5', 'c6'],
+  
+  // 1. e4 e5 2. Nf3 Nc6
   'r1bqkbnr/pppp1ppp/2n5/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 2 3': ['Bb5', 'Bc4', 'd4', 'Nc3'],
+  
+  // 1. e4 e5 2. Nf3 Nf6 (Petrov)
   'rnbqkb1r/pppp1ppp/5n2/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 2 3': ['Nxe5', 'd4', 'Bc4', 'Nc3'],
+  
+  // 1. e4 c5 2. Nf3
+  'rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2': ['d6', 'Nc6', 'e6', 'g6'],
+  
+  // 1. e4 c5 2. Nf3 d6
   'rnbqkbnr/pp2pppp/3p4/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 0 3': ['d4', 'Bb5+', 'Nc3'],
-  'rnbqkbnr/pp1p1ppp/4p3/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 0 3': ['d4', 'Nc3', 'd3'],
+  
+  // 1. e4 c5 2. Nf3 Nc6
+  'r1bqkbnr/pp1ppppp/2n5/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 1 3': ['d4', 'Bb5', 'Nc3', 'd3'],
+  
+  // 1. e4 e6 2. d4
+  'rnbqkbnr/pppp1ppp/4p3/8/3PP3/8/PPP2PPP/RNBQKBNR b KQkq - 0 2': ['d5', 'c5', 'Nf6'],
+  
+  // 1. e4 e6 2. d4 d5
+  'rnbqkbnr/ppp2ppp/4p3/3p4/3PP3/8/PPP2PPP/RNBQKBNR w KQkq - 0 3': ['Nc3', 'Nf3', 'Nd2', 'e5'],
+  
+  // 1. e4 c6 2. d4
+  'rnbqkbnr/pp1ppppp/2p5/8/3PP3/8/PPP2PPP/RNBQKBNR b KQkq - 0 2': ['d5', 'd6', 'e5'],
+  
+  // 1. e4 c6 2. d4 d5
+  'rnbqkbnr/pp2pppp/2p5/3p4/3PP3/8/PPP2PPP/RNBQKBNR w KQkq - 0 3': ['Nc3', 'Nf3', 'Nd2', 'e5'],
+  
+  // 1. d4 d5 2. c4
+  'rnbqkbnr/ppp1pppp/8/3p4/2PP4/8/PP2PPPP/RNBQKBNR b KQkq - 0 2': ['e6', 'c6', 'dxc4', 'Nf6'],
+  
+  // 1. d4 d5 2. c4 e6 (Gambito de Dama)
   'rnbqkbnr/ppp2ppp/4p3/3p4/2PP4/8/PP2PPPP/RNBQKBNR w KQkq - 0 3': ['Nc3', 'Nf3', 'Bg5', 'cxd5'],
+  
+  // 1. d4 d5 2. c4 c6 (Eslava)
   'rnbqkbnr/pp2pppp/2p5/3p4/2PP4/8/PP2PPPP/RNBQKBNR w KQkq - 0 3': ['Nf3', 'Nc3', 'Bg5', 'cxd5'],
+  
+  // 1. d4 Nf6 2. c4
+  'rnbqkb1r/pppppppp/5n2/8/2PP4/8/PP2PPPP/RNBQKBNR b KQkq - 0 2': ['e6', 'g6', 'c5', 'd6'],
+  
+  // 1. d4 Nf6 2. c4 e6 (Nimzo-India)
+  'rnbqkb1r/pppp1ppp/4pn2/8/2PP4/8/PP2PPPP/RNBQKBNR w KQkq - 0 3': ['Nc3', 'Nf3', 'Bg5'],
+  
+  // 1. d4 Nf6 2. c4 g6 (India de Rey)
+  'rnbqkb1r/pppppp1p/5np1/8/2PP4/8/PP2PPPP/RNBQKBNR w KQkq - 0 3': ['Nc3', 'Nf3', 'g3'],
+  
+  // 1. e4 e5 2. Nf3 Nc6 3. Bb5 (Española)
   'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3': ['a6', 'Nf6', 'd6', 'Bc5', 'Nge7'],
+  
+  // 1. e4 e5 2. Nf3 Nc6 3. Bc4 (Italiana)
   'r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3': ['Bc5', 'Nf6', 'd6', 'f5'],
+  
+  // 1. e4 e5 2. Nf3 Nc6 3. d4 (Escocesa)
   'r1bqkbnr/pppp1ppp/2n5/4p3/3PP3/5N2/PPP2PPP/RNBQKB1R b KQkq - 0 3': ['exd4', 'Nf6', 'Bc5', 'd6'],
+  
+  // 1. e4 e5 2. Nf3 Nf6 3. Nxe5 (Petrov)
   'rnbqkb1r/pppp1ppp/5n2/4N3/4P3/8/PPPP1PPP/RNBQKB1R b KQkq - 0 3': ['d6', 'Nc6', 'Nxe4', 'Bc5'],
-  'rnbqkb1r/pppp1ppp/5n2/4p3/3PP3/5N2/PPP2PPP/RNBQKB1R b KQkq - 0 3': ['exd4', 'Nc6', 'Bc5']
+  
+  // 1. e4 c5 2. Nf3 d6 3. d4
+  'rnbqkbnr/pp2pppp/3p4/2p5/3PP3/5N2/PPP2PPP/RNBQKB1R b KQkq - 0 3': ['cxd4', 'Nf6', 'Nc6'],
+  
+  // 1. e4 c5 2. Nf3 Nc6 3. d4
+  'r1bqkbnr/pp1ppppp/2n5/2p5/3PP3/5N2/PPP2PPP/RNBQKB1R b KQkq - 0 3': ['cxd4', 'Nf6', 'g6'],
+  
+  // 1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 (Española Morphy)
+  'r1bqkbnr/1ppp1ppp/2n5/p3p3/1B2P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 0 4': ['Ba4', 'Bxc6', 'O-O'],
+  
+  // 1. e4 e5 2. Nf3 Nc6 3. Bb5 Nf6 (Berlín)
+  'r1bqkb1r/pppp1ppp/2n2n2/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 2 4': ['O-O', 'd3', 'Nc3'],
+  
+  // 1. e4 e5 2. Nf3 Nc6 3. Bc4 Bc5 (Italiana Giuoco Piano)
+  'r1bqk1nr/pppp1ppp/2n5/2b1p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 2 4': ['c3', 'd3', 'O-O', 'Nc3'],
+  
+  // 1. e4 e5 2. Nf3 Nc6 3. Bc4 Nf6 (Dos Caballos)
+  'r1bqkb1r/pppp1ppp/2n2n2/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 2 4': ['Ng5', 'd3', 'Nc3', 'O-O'],
+  
+  // 1. d4 d5 2. c4 e6 3. Nc3
+  'rnbqkbnr/ppp2ppp/4p3/3p4/2PP4/2N5/PP2PPPP/R1BQKBNR b KQkq - 1 3': ['Nf6', 'c5', 'Bb4', 'dxc4'],
+  
+  // 1. d4 d5 2. c4 e6 3. Nf3
+  'rnbqkbnr/ppp2ppp/4p3/3p4/2PP4/5N2/PP2PPPP/RNBQKB1R b KQkq - 1 3': ['Nf6', 'c5', 'Be7', 'dxc4'],
+  
+  // 1. d4 Nf6 2. c4 e6 3. Nc3
+  'rnbqkb1r/pppp1ppp/4pn2/8/2PP4/2N5/PP2PPPP/R1BQKBNR b KQkq - 1 3': ['Bb4', 'd5', 'c5', 'Be7'],
+  
+  // 1. d4 Nf6 2. c4 g6 3. Nc3
+  'rnbqkb1r/pppppp1p/5np1/8/2PP4/2N5/PP2PPPP/R1BQKBNR b KQkq - 1 3': ['Bg7', 'd5', 'd6'],
+  
+  // 1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 4. Ba4
+  'r1bqkbnr/1ppp1ppp/2n5/p3p3/B3P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 1 4': ['Nf6', 'b5', 'd6', 'Bc5'],
+  
+  // 1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 4. Bxc6
+  'r1bqkbnr/1ppp1ppp/2B5/4p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 0 4': ['dxc6', 'bxc6'],
+  
+  // 1. e4 e5 2. Nf3 Nc6 3. Bc4 Bc5 4. c3
+  'r1bqk1nr/pppp1ppp/2n5/2b1p3/2B1P3/2P2N2/PP1P1PPP/RNBQK2R b KQkq - 0 4': ['Nf6', 'd6', 'Qe7'],
+  
+  // 1. e4 e5 2. Nf3 Nc6 3. Bc4 Bc5 4. d3
+  'r1bqk1nr/pppp1ppp/2n5/2b1p3/2BPP3/5N2/PPP2PPP/RNBQK2R b KQkq - 0 4': ['Nf6', 'd6', 'Nf6'],
+  
+  // 1. d4 d5 2. c4 e6 3. Nc3 Nf6
+  'rnbqkb1r/ppp2ppp/4pn2/3p4/2PP4/2N5/PP2PPPP/R1BQKBNR w KQkq - 2 4': ['Bg5', 'Nf3', 'cxd5'],
+  
+  // 1. d4 d5 2. c4 e6 3. Nf3 Nf6
+  'rnbqkb1r/ppp2ppp/4pn2/3p4/2PP4/5N2/PP2PPPP/RNBQKB1R w KQkq - 2 4': ['Nc3', 'Bg5', 'cxd5'],
+  
+  // 1. d4 Nf6 2. c4 e6 3. Nc3 Bb4 (Nimzo-India)
+  'rnbqk2r/pppp1ppp/4pn2/8/1bPP4/2N5/PP2PPPP/R1BQKBNR w KQkq - 1 4': ['Qc2', 'e3', 'Nf3', 'Bg5'],
+  
+  // 1. d4 Nf6 2. c4 g6 3. Nc3 Bg7 (India de Rey)
+  'rnbqk2r/ppppppbp/5np1/8/2PP4/2N5/PP2PPPP/R1BQKBNR w KQkq - 1 4': ['e4', 'Nf3', 'g3', 'e3']
 };
-
 // TIEMPOS OPTIMIZADOS PARA MÓVIL
 const LEVELS = {
   600:  { depth: 1, noise: 250, blunder: 0.25, quiescence: false, iterDeep: false, time: 50 },
