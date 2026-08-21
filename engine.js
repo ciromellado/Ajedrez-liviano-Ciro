2268c3b530978a5ce730f7ef64a2e0fc97973e5a
ciromellado <ciromellado@gmail.com>
2026-08-21 07:41:06 -0400

    Adjust noise parameters for different levels

--- engine.js
+++ engine.js
@@ -273,9 +273,9 @@ const OPENING_BOOK = {
 };
 // TIEMPOS OPTIMIZADOS PARA MÓVIL
 const LEVELS = {
-  600:  { depth: 1, noise: 250, blunder: 0.25, quiescence: false, iterDeep: false, time: 50 },
-  1200: { depth: 2, noise: 80,  blunder: 0.05, quiescence: false, iterDeep: false, time: 150 },
-  1300: { depth: 3, noise: 30,  blunder: 0.02, quiescence: false, iterDeep: false, time: 800 },
+  600:  { depth: 1, noise: 100, blunder: 0.25, quiescence: false, iterDeep: false, time: 50 },
+  1200: { depth: 2, noise: 40,  blunder: 0.05, quiescence: false, iterDeep: false, time: 150 },
+  1300: { depth: 3, noise: 10,  blunder: 0.02, quiescence: false, iterDeep: false, time: 800 },
   1500: { depth: 4, noise: 0,   blunder: 0.0,  quiescence: false, iterDeep: false, time: 1200 }
 };
 
